a0b176e72c29ea046e4537718e1cf00bb2889d3d
bgdnvk <bnovykov@gmail.com>
2022-04-20 23:50:53 +0200

    async await blogs

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,17 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+//promise
+// const getAll = () => {
+//   const request = axios.get(baseUrl)
+//   return request.then(response => response.data)
+// }
+
+//async/await
+const getAll = async () => {
+  const request = await axios.get(baseUrl)
+  console.log(request)
+  return request.data
 }
 
 export default { getAll }
