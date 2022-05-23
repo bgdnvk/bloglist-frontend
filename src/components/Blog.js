711fc22c42cc1e05929e9e8bab73aec973e780ca
bgdnvk <bnovykov@gmail.com>
2022-05-23 23:07:57 +0200

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {React, useState} from 'react'
+
+
+
+const Blog = ({blog}) => {
+  //some CSS
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  //flag to show or hide details
+  const [view, setView] = useState(false)
+
+  //function to view/hide a blog post
+  const handleView = e => {
+    e.preventDefault()
+    setView(!view)
+  }
+  //
+  if(!view) {
+    return(
+      <div style={blogStyle}>
+         {blog.title} 
+         {blog.author} 
+         <button onClick={handleView}>view</button>
+      </div> 
+    ) 
+  }
+  return(
+    <div style={blogStyle}>
+      {blog.title} 
+      {blog.author} 
+      {blog.likes} 
+      {blog.url} 
+      <button onClick={handleView}>hide</button>
+    </div>
+  )
+}
 
 export default Blog
