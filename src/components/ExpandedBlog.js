7da13c67dbdeadf98897ea0dccdbbc7f5704ff81
bgdnvk <bnovykov@gmail.com>
2022-06-05 21:25:02 +0200

    5.8 comments

--- src/components/ExpandedBlog.js
+++ src/components/ExpandedBlog.js
@@ -2,13 +2,17 @@ import blogService from "../services/blogs";
 
 const ExpandedBlog = ({blog, blogStyle, handleView, likes, setLikes}) => {
 
-
+    //5.8 is weird, check L11
     const handleLike = async (e) => {
         //debug like a pro
         console.log('like btn pressed');
         console.log('e from the like btn is ', e)
 
         //add use info to the db, altho it's included in the schema
+        //this doesn't make sense as the blog already has a user?
+        //so if you like a blog post the user would change?
+        //5.8 wut?
+        //it works tho
         const loggedUserJSON = window.localStorage.getItem('loggedUser')
         const user = JSON.parse(loggedUserJSON)
         console.log('loggedUserJSON is ', user)
@@ -32,8 +36,8 @@ const ExpandedBlog = ({blog, blogStyle, handleView, likes, setLikes}) => {
         }
     }
 
+    //TODO: update blogs
     const handleDelete = async (e) => {
-        //TODO: update blogs
         console.log('delete button pressed')
         try{
             if(window.confirm('do you want to delete?')){
