8765ce34364f9fbb26be19dd76083c87e7c977fb
bgdnvk <bnovykov@gmail.com>
2022-06-10 04:23:11 +0200

    5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import HiddenBlog from './HiddenBlog'
+
+const blog = {
+  title: 'test blog',
+  author: 'test admin',
+  likes: 5,
+  url: 'www.test.com',
+}
 
 test('render blog', () => {
-  const blog = {
-    title: 'test blog',
-    author: 'test admin',
-    likes: 5,
-    url: 'www.test-com',
-  }
 
   //   render(<Blog blog={blog}></Blog>)
   //   screen.debug()
@@ -21,5 +24,34 @@ test('render blog', () => {
   screen.debug()
 
   expect(div).toHaveTextContent('test blog')
+
+})
+
+test('clicking btn HiddenBlog', async () => {
+
+  const mockHandler = jest.fn()
+
+  render(
+    <HiddenBlog blog={blog} handleView={mockHandler}></HiddenBlog>
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
+
+test('click btn and likes n url shown', async () => {
+
+  const container = render(<Blog blog={blog}></Blog>).container
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+  const div = container.querySelector('.expandedBlog')
+  screen.debug()
+  expect(div).toHaveTextContent('www.test.com')
+  expect(div).toHaveTextContent('likes 5')
 
 })
